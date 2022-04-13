3c31bf0808194570e2566eeb8c98ddf12053cd7b
Qianmo <wang_yuxuan@bupt.edu.cn>
2022-04-13 21:08:59 +0800

    Revert "IP_C"
    This reverts commit 58fb968e6d233882abda01f068b4df662e2db5f9.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import func
-import data
 
+def print_hi(name):
+    # 在下面的代码行中使用断点来调试脚本。
+    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+
+
+# 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    while True:
-        code, instr = func.IP_C()
-        print(code, instr)
+    print_hi('PyCharm')
