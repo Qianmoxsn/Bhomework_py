c72b8bd9fa17c2d7570544f77fb3b08715369c67
Qianmo <wang_yuxuan@bupt.edu.cn>
2022-04-18 21:21:13 +0800

    merge：basic version of FCFS
    commit d97dab0f9bdaec6897c44a4ddb13a05ec877f15a
    Author: Qianmo <wang_yuxuan@bupt.edu.cn>
    Date:   Mon Apr 18 21:17:49 2022 +0800
    
        doc:添加注释，格式化代码
    
    commit b1cff0411848e65697a06223f61c09ccf8459d28
    Author: Qianmo <wang_yuxuan@bupt.edu.cn>
    Date:   Mon Apr 18 20:27:09 2022 +0800
    
        fix:"FCFS"
    
    commit fa6f425c01db81cb684ef113c4d0d8be16ecc60b
    Author: Qianmo <wang_yuxuan@bupt.edu.cn>
    Date:   Mon Apr 18 18:33:33 2022 +0800
    
        feat:oj版全功能
    
    commit b993d6b99462d8ff29b484c1eeebf338ba477fa0
    Author: Qianmo <wang_yuxuan@bupt.edu.cn>
    Date:   Sun Apr 17 20:31:51 2022 +0800
    
        feat:ipc ipce timr
    
    commit 7e6866dc10fa6a6e090fb9247ec5dfc101581db3
    Author: kkdbg666 <1134517405@qq.com>
    Date:   Sun Apr 17 18:37:13 2022 +0800
    
        Update funcs.py

--- main.py
+++ main.py
@@ -5,27 +5,34 @@ if __name__ == '__main__':
 
     # 输入配置参数并配置
     gl_VAR.g_totsta, gl_VAR.g_stg, gl_VAR.g_dis = funcs.SET_CONFIG()
-
     data.CONFIG(nsta=gl_VAR.g_totsta, stg=gl_VAR.g_stg, dis=gl_VAR.g_dis)
 
     # 初始化data.py中的变量
-    SED_LST = data.SED_LST(0, '0' * gl_VAR.g_totsta)
-    BUS_CON = data.BUS_CON(0, 0, 0, 0)
-    STA_CON = data.STA_CON('0' * gl_VAR.g_totsta, '0' * gl_VAR.g_totsta)
+    data.BUS_CON.setvar(0, 0, 0, '0' * gl_VAR.g_totsta)
+    data.STA_CON.setvar('0' * gl_VAR.g_totsta, '0' * gl_VAR.g_totsta)
 
-    funcs.OP_C(BUS_CON, STA_CON)
     # 运行
+    funcs.OP_C(data.BUS_CON, data.STA_CON)  # 第一次运行，输出结果
     while True:
         code, instr = funcs.IP_C()
         if code == -2:
             continue
         if code == -1:
-            funcs.OP_C_E(STA_CON)
+            funcs.OP_C_E()
             exit(-1)
-        elif code == 0:
-            funcs.OP_C(BUS_CON, STA_CON)
-            funcs.TIMR()
-            funcs.BUS_ACT().BUS_MOV()
         elif code == 1 or code == 2 or code == 3:
-            funcs.BUS_ACT().PSG_D()
-            funcs.BUS_ACT().PSG_U()
+            # 执行STG函数
+            intrpos = funcs.STG(code, instr)
+            if intrpos == 'ST_BY':
+                pass
+            # 执行ADD_SED函数
+            funcs.ADD_SED(code, instr, intrpos)
+            # print("<-sedlst->\n", data.SED_LST)
+        elif code == 0:
+            # 执行BUS_MOV函数
+            if funcs.BUS_MOV() == 'ST_BY':
+                pass
+            # 执行TIMR函数
+            funcs.TIMR()
+            # 执行OP_C函数
+            funcs.OP_C(data.BUS_CON, data.STA_CON)
